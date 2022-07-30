b084a392f191c6fff6711288790effd3890c820f
M.A.I <mario.imperato@gpagroup.it>
2022-07-30 13:07:32 +0200

    Added batisutil folder

---
+++ batisutil/strings.go
@@ -0,0 +1,25 @@
+package batisutil
+
+import "fmt"
+
+func ToMaxText(s string, maxLength int) (string, bool) {
+	if maxLength == 0 {
+		panic(fmt.Sprintf("tpm-batis::ToMaxText - maxLength cannot be 0 (%d)", maxLength))
+	}
+
+	truncated := false
+	absMaxLength := maxLength
+	if absMaxLength < 0 {
+		absMaxLength = -maxLength
+	}
+	if len(s) > absMaxLength {
+		truncated = true
+		if maxLength > 0 {
+			s = s[0:maxLength]
+		} else {
+			s = s[len(s)+maxLength:]
+		}
+	}
+
+	return s, truncated
+}
---
+++ batisutil/tx.go
@@ -0,0 +1,27 @@
+package batisutil
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/rs/zerolog/log"
+)
+
+func TxRollback(tx *sqlx.Tx) {
+	err := tx.Rollback()
+	if err != nil {
+		log.Error().Err(err).Msg("transaction rollback")
+	}
+}
+
+func TxClose(tx *sqlx.Tx, err error) error {
+	if err == nil {
+		err = tx.Commit()
+		if err != nil {
+			log.Error().Err(err).Msg("transaction commit")
+		}
+
+		return err
+	}
+
+	TxRollback(tx)
+	return err
+}
